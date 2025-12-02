0dfbc162cbb87896f7995dbf7ef102ec55b25172
Zakary Macejka <zakaxm11@gmail.com>
2025-12-02 18:45:56 -0500

    Exclude DataSourceAutoConfiguration in tests
    Updated SpringBootTest annotation to exclude DataSourceAutoConfiguration and added comment to contextLoads test.

--- src/test/java/com/example/demo/SpringHelloMysqlApplicationTests.java
+++ src/test/java/com/example/demo/SpringHelloMysqlApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootTest
+@SpringBootTest(exclude = { DataSourceAutoConfiguration.class })
 class SpringHelloMysqlApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // test will pass without connecting to MySQL
+    }
 
 }
