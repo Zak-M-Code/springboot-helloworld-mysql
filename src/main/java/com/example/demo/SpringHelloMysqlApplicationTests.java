c849690f5ce1ab580f62f5205ed51286cdb5df69
Zak-M-Code <macejkaz1@montclair.edu>
2025-12-02 18:51:51 -0500

    Upgrade Spring Boot to 3.2.2 to fix @SpringBootTest exclude issue

---
+++ src/main/java/com/example/demo/SpringHelloMysqlApplicationTests.java
@@ -0,0 +1,17 @@
+ GNU nano 7.2               SpringHelloMysqlApplicationTests.java                         
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
+class SpringHelloMysqlApplicationTests {
+
+}
+
